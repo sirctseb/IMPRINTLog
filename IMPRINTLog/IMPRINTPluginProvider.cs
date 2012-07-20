d86703645f23736fc1dbd74fdc06a20ead24bd48
Christopher Best <bestchris@gmail.com>
2012-07-20 12:07:30 -0400

    Started Log project from template

---
+++ IMPRINTLog/IMPRINTPluginProvider.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using MAAD.Plugins.ImprintPluginLoader;
+
+[assembly: ImprintPluginAttribute()]
+
+
+namespace IMPRINTLog
+{
+	public class IMPRINTLogPlugin : IExternalVariableProvider, IImprintPlugin
+	{
+		#region IExternalVariableProvider Implementation
+
+		public string[] GetAssemblyReferences()
+		{
+			return new string[]
+			{
+				"IMPRINTLog.dll"
+			};
+		}
+
+		public IEnumerable<VariableName> GetExternalVariables()
+		{
+			return new VariableName[]
+			{
+				new VariableName("IMPRINTLog", typeof(IMPRINTLog))
+			};
+		}
+
+		public string[] GetNamespaceAliases()
+		{
+			return new string[]
+			{
+				// "IMPRINTLog"
+			};
+		}
+
+		#endregion
+
+		#region IImprintPlugin Implementation
+		
+		public string Author
+		{
+			get { return "Christopher Best"; }
+		}
+
+		public DateTime Date
+		{
+			get { return DateTime.Today; }
+		}
+
+		public string Description
+		{
+			get { return "A logging class for IMPRINT"; }
+		}
+
+		public string Name
+		{
+			get { return "IMPRINTLog"; }
+		}
+
+		#endregion
+	}
+}
