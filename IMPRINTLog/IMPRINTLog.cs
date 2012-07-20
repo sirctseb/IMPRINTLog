3799d74155a7e9c140a1823a1c73bd23dc7204a4
Christopher Best <bestchris@gmail.com>
2012-07-20 12:22:53 -0400

    defined console interface and output window class
    they should probably not be in the same file
    added a console property to log class
    implemented default constructor

--- IMPRINTLog/IMPRINTLog.cs
+++ IMPRINTLog/IMPRINTLog.cs
@@ -12,11 +12,35 @@ namespace IMPRINTLog
 			get { return MAAD.Simulator.Generator.StaticGenerator.SimulationApplication; }
 		}
 	}
+
+	// Interface for classes that we can log to
+	public interface ILogConsole
+	{
+		// Accept an object to add to the log
+		void AcceptTrace(object obj);
+	}
+
+	// Class for logging to IMPRINT output window
+	public class IMPRINTOutputWindow : SmartPlugin, ILogConsole
+	{
+		public void AcceptTrace(object obj)
+		{
+			app.AcceptTrace(obj);
+		}
+	}
+
 	public class IMPRINTLog : SmartPlugin
 	{
+		public ILogConsole console
+		{
+			get; set;
+		}
+
+		// default constructor
 		public IMPRINTLog()
 		{
 			// initialize default logger
+			console = new IMPRINTOutputWindow();
 		}
 	}
 }
